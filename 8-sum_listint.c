39838e36abd2e7f7be4206835f077409b4751789
aminatdamilola <aminatdamilola50@gmail.com>
2022-11-30 02:32:01 -0800

    Task 8

---
+++ 8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of all the data(n) of linked list
+ * @head: head of a list
+ * Return: sum of all data(n)
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ my first time in c programming
 # alx-low_level_programming
 My first time in C programming language 
 >>>>>>> ca62d317d65260dbc697dfe9c2f70bf209d6e31e
+recursion
+more malloc free
