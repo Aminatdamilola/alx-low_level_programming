7e8aea863a71edcdfe8f46405d4d53a3b16df0c1
aminatdamilola <aminatdamilola50@gmail.com>
2023-01-19 06:58:21 -0800

    task 0

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - conpute the absolute value of an integer
+ * @r: the integer to be computed
+ * Return: absolute value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
+
